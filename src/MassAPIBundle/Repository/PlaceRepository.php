c85171c70713f903af6d53af36bdae2ea3d74350
Laurent <laurent@ghirardotti.fr>
2016-07-16 17:26:38 +0200

    Fix Doctrine request for place

--- src/MassAPIBundle/Repository/PlaceRepository.php
+++ src/MassAPIBundle/Repository/PlaceRepository.php
@@ -9,9 +9,12 @@ class PlaceRepository extends EntityRepository
     public function getNearPlace(array $criteria = array())
     {
         return $this->createQueryBuilder('p')
+            ->addSelect('p', 'g')
             ->leftJoin('p.geo', 'g')
-            ->andWhere('CONTAINS(GeomFromJson(:inside_json), p.geoPoint) > 0')
+            ->andWhere('CONTAINS(GeomFromJson(:inside_json), g.geoPoint) > 0')
             ->setParameter('inside_json', $criteria['inside_json'])
+            ->getQuery()
+            ->getResult()
             ;
     }
 }

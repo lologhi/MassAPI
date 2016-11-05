f84e49f8f927547899c59b91a4a373f878d88df8
Laurent <laurent@ghirardotti.fr>
2016-11-05 19:35:39 +0800

    Cleanup and look at different distance from city center

--- src/MassAPIBundle/Command/OpenStreetMapQueryCommand.php
+++ src/MassAPIBundle/Command/OpenStreetMapQueryCommand.php
@@ -4,9 +4,10 @@ namespace MassAPIBundle\Command;
 
 
 use Doctrine\ORM\EntityManagerInterface;
-use Guzzle\Http\Client;
+use GuzzleHttp\Client;
 use KageNoNeko\OSM\BoundingBox;
 use KageNoNeko\OSM\OverpassConnection;
+use MassAPIBundle\Entity\Place;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -28,57 +29,81 @@ class OpenStreetMapQueryCommand extends ContainerAwareCommand
         $places = $em->getRepository('MassAPIBundle:Place')->findBy(array('name' => 'Eglise'));
 
         /** @var Client $client */
-        $client = $this->getContainer()->get('csa_guzzle.client.osm_api');
+        $guzzleClient = $this->getContainer()->get('csa_guzzle.client.osm_api');
 
         foreach ($places as $place) {
-            $results = $this->queryNode($place->getGeo()->getLatitude(),$place->getGeo()->getLongitude());
-            if (! empty($results['elements'])) {
-                foreach ($results['elements'] as $result) {
-                    dump($result);
-                    if (array_key_exists('tags', $result) && array_key_exists('name', $result['tags'])) {
-                        $place->setName($result['tags']['name']);
-                    }
-                    if (array_key_exists('lat', $result) && array_key_exists('lon', $result)) {
-                        $place->getGeo()->setLatitude($result['lat']);
-                        $place->getGeo()->setLongitude($result['lon']);
-                    }
+            $data = array();
+            $variation = 0.00;
+
+            while (empty($data)) {
+                $variation += 0.01;
+                $output->writeln('Looking for unnamed church in '.$place->getAddress()->getAddressLocality() . ' ('.$variation.')');
+
+                $results = $this->queryNode($place, $variation);
+                $data = $this->processResult($results, $guzzleClient);
+
+                if (empty($data)) {
+                    $results = $this->queryWay($place, $variation);
+                    $data = $this->processResult($results, $guzzleClient);
                 }
             }
 
-            $results = $this->queryWay($place->getGeo()->getLatitude(),$place->getGeo()->getLongitude());
-            if (! empty($results['elements'])) {
-                foreach ($results['elements'] as $result) {
-                    dump($result);
-                    if (array_key_exists('tags', $result) && array_key_exists('name', $result['tags'])) {
-                        $place->setName($result['tags']['name']);
-                    }
-                    if (array_key_exists('nodes', $result) && !empty($result['nodes'])) {
-                        $result = $client->get('node/' . $result['nodes'][0]);
-                        $node = simplexml_load_string($result->getBody()->getContents());
-                        $attributes = $node->node[0]->attributes();
-                        if (array_key_exists('lat', $attributes) && array_key_exists('lon', $attributes)) {
-                            $place->getGeo()->setLatitude($attributes['lat']);
-                            $place->getGeo()->setLongitude($attributes['lon']);
-                        }
-                    }
-                }
+
+            if (array_key_exists('name', $data)) {
+                $output->writeln('Church found: '.$data['name'].' ('.$variation.')');
+            } else {
+                $output->writeln('Found one, unnamed in OSM :\'(');
             }
+
+            /*$place->setName($data['name']);
+            $place->getGeo()->setLatitude($data['lat']);
+            $place->getGeo()->setLongitude($data['lon']);*/
         }
     }
 
-    private function queryNode($latitude, $longitude)
+    private function processResult($results, Client $guzzleClient)
     {
-        return $this->query($latitude, $longitude, 'node');
+        $data = array();
+
+        if (empty($results['elements'])) {
+            return $data;
+        }
+
+        foreach ($results['elements'] as $result) {
+            if (array_key_exists('tags', $result) && array_key_exists('name', $result['tags'])) {
+                $data['name'] = $result['tags']['name'];
+            }
+            if (array_key_exists('lat', $result) && array_key_exists('lon', $result)) {
+                $data['lat'] = $result['lat'];
+                $data['lon'] = $result['lon'];
+            }
+            if (array_key_exists('type', $result) && 'way' === $result['type']) {
+                // Query OSM API node
+                $result = $guzzleClient->get('node/' . $result['nodes'][0]);
+                $node = simplexml_load_string($result->getBody()->getContents());
+                $attributes = $node->node[0]->attributes();
+                if ($attributes->lat && $attributes->lon) {
+                    $data['lat'] = $attributes['lat'];
+                    $data['lon'] = $attributes['lon'];
+                }
+            }
+        }
+
+        return $data;
     }
 
-    private function queryWay($latitude, $longitude)
+    private function queryNode(Place $place, $variation)
     {
-        return $this->query($latitude, $longitude, 'way');
+        return $this->query($place->getGeo()->getLatitude(), $place->getGeo()->getLongitude(), 'node', $variation);
     }
 
-    private function query($latitude, $longitude, $type)
+    private function queryWay(Place $place, $variation)
     {
-        $variation = 0.03;
+        return $this->query($place->getGeo()->getLatitude(), $place->getGeo()->getLongitude(), 'way', $variation);
+    }
+
+    private function query($latitude, $longitude, $type, $variation)
+    {
         $lowerLatitude   = $latitude - $variation;
         $higherLatitude  = $latitude + $variation;
         $lowerLongitude  = $longitude - $variation;
